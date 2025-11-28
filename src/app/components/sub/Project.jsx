e2e3d9dfa9016825788b6f6b087352982101314c
Soe Nay Lin <soenaylin.g@gmail.com>
2025-11-28 12:10:27 +0630

    add link to project component to connect simple project deployment

--- src/app/components/sub/Project.jsx
+++ src/app/components/sub/Project.jsx
@@ -8,31 +8,35 @@ export default function Project({ data, index }) {
 	const [show, setShow] = useState(false);
 
 	return (
-		<motion.div
-			initial={{ opacity: 0, y: index % 2 === 0 ? 100 : -100 }}
-			whileInView={{ opacity: 1, y: 0 }}
-			viewport={{ once: true }}
-			transition={{ duration: 1, type: "spring", stiffness: 100 }}
-			onClick={() => setShow(!show)}
-			className="relative w-[350px] max-[639px]:w-full h-max border border-red-300 rounded-lg cursor-pointer shadow-2xl">
-			<Image
-				src={data.url}
-				alt="Project Image"
-				width={400}
-				height={400}
-				className="rounded-lg opacity-95"
-			/>
+		<a
+			href={data.link}
+			target="_blank">
 			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: show ? 1 : 0 }}
-				className="absolute top-0 w-full h-full flex flex-col items-center justify-center gap-y-2 bg-white/95 p-6 rounded-lg">
-				<h2 className="text-lg font-bold tracking-wide text-gray-500">
-					{data.name}
-				</h2>
-				<p className="text-justify text-gray-500 first-letter:pl-2">
-					{data.desc}
-				</p>
+				initial={{ opacity: 0, y: index % 2 === 0 ? 100 : -100 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				viewport={{ once: true }}
+				transition={{ duration: 1, type: "spring", stiffness: 100 }}
+				// onClick={() => setShow(!show)}
+				className="relative w-[350px] max-[639px]:w-full h-max border border-red-300 rounded-lg cursor-pointer shadow-2xl">
+				<Image
+					src={data.url}
+					alt="Project Image"
+					width={400}
+					height={400}
+					className="rounded-lg opacity-95"
+				/>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: show ? 1 : 0 }}
+					className="absolute top-0 w-full h-full flex flex-col items-center justify-center gap-y-2 bg-white/95 p-6 rounded-lg">
+					<h2 className="text-lg font-bold tracking-wide text-gray-500">
+						{data.name}
+					</h2>
+					<p className="text-justify text-gray-500 first-letter:pl-2">
+						{data.desc}
+					</p>
+				</motion.div>
 			</motion.div>
-		</motion.div>
+		</a>
 	);
 }
